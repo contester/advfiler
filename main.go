153fb0b03767787dea2c7aa90257bdf9fc8f5a79
Paul Komkoff <i@stingr.net>
2016-03-15 18:54:14 +0300

    .

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"sort"
+	"strconv"
 	"strings"
 
 	"gopkg.in/redis.v3"
@@ -136,6 +137,34 @@ func (f *filerServer) handleList(w http.ResponseWriter, r *http.Request, path st
 	return json.NewEncoder(w).Encode(&wr)
 }
 
+func addDigests(h http.Header, digests map[string]string) {
+	var dkeys []string
+	for k := range digests {
+		dkeys = append(dkeys, k)
+	}
+	sort.Strings(dkeys)
+	var dvals []string
+	for _, k := range dkeys {
+		dvals = append(dvals, k+"="+digests[k])
+	}
+	h.Add("Digest", strings.Join(dvals, ","))
+	if md5, ok := digests["MD5"]; ok && md5 != "" {
+		h.Add("Content-MD5", md5)
+	}
+}
+
+func parseDigests(dh string) map[string]string {
+	result := make(map[string]string)
+	for _, v := range strings.Split(dh, ",") {
+		ds := strings.SplitN(strings.TrimSpace(v), "=", 2)
+		if len(ds) != 2 {
+			continue
+		}
+		result[strings.ToUpper(ds[0])] = ds[1]
+	}
+	return result
+}
+
 func (f *filerServer) handleDownload(w http.ResponseWriter, r *http.Request, path string) error {
 	if path == "" || path[len(path)-1] == '/' {
 		return f.handleList(w, r, path)
@@ -144,21 +173,8 @@ func (f *filerServer) handleDownload(w http.ResponseWriter, r *http.Request, pat
 	if err != nil {
 		return err
 	}
-
-	var dkeys []string
-	for k := range fi.Digests {
-		dkeys = append(dkeys, k)
-	}
-	sort.Strings(dkeys)
-	var dvals []string
-	for _, k := range dkeys {
-		dvals = append(dvals, k+"="+fi.Digests[k])
-	}
-	w.Header().Add("Digest", strings.Join(dvals, ","))
-	if md5, ok := fi.Digests["MD5"]; ok && md5 != "" {
-		w.Header().Add("Content-MD5", md5)
-	}
-	w.Header().Add("Content-Length", fmt.Sprintf("%d", fi.Size))
+	addDigests(w.Header(), fi.Digests)
+	w.Header().Add("Content-Length", strconv.FormatInt(fi.Size, 10))
 
 	for _, ch := range fi.Chunks {
 		volurl, err := lookupVolUrl(f.weedMaster, ch.Fid)
@@ -198,16 +214,13 @@ func redisKeyToPath(key string) (string, error) {
 }
 
 func (f *filerServer) getFileInfo(path string) (*redisFileInfo, error) {
-	st := f.redisClient.Get(redisKey(path))
-	if st.Err() != nil {
-		return nil, st.Err()
-	}
-	ust, err := st.Bytes()
+	res, err := f.redisClient.Get(redisKey(path)).Result()
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("%s %d %d\n", path, len(res), strings.IndexByte(res, 0))
 	var fi redisFileInfo
-	if err = json.Unmarshal(ust, &fi); err != nil {
+	if err = json.Unmarshal([]byte(res), &fi); err != nil {
 		return nil, err
 	}
 	return &fi, nil
@@ -220,21 +233,38 @@ func (f *filerServer) handleDelete(w http.ResponseWriter, r *http.Request, path 
 	}
 
 	for _, ch := range fi.Chunks {
-		volurl, err := lookupVolUrl(f.weedMaster, ch.Fid)
-		if err != nil {
+		if err = f.deleteChunkFid(ch.Fid); err != nil {
 			return err
 		}
-		req, err := http.NewRequest("DELETE", "http://"+volurl+"/"+ch.Fid, nil)
-		if err != nil {
-			return err
-		}
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return err
-		}
-		resp.Body.Close()
 	}
 	return f.redisClient.Del(redisKey(path)).Err()
+}
+
+func deleteChunkURL(url string) error {
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	return nil
+}
+
+func (f *filerServer) deleteChunkFid(fid string) error {
+	volurl, err := lookupVolUrl(f.weedMaster, fid)
+	if err != nil {
+		return err
+	}
+	return deleteChunkURL("http://" + volurl + "/" + fid)
+}
+
+func (f *filerServer) deleteChunks(chunks []redisChunk) {
+	for _, v := range chunks {
+		f.deleteChunkFid(v.Fid)
+	}
 }
 
 func (f *filerServer) handleUpload(w http.ResponseWriter, r *http.Request, path string) error {
@@ -256,12 +286,23 @@ func (f *filerServer) handleUpload(w http.ResponseWriter, r *http.Request, path 
 		"SHA": sha1.New(),
 	}
 
+	contentLength := int64(-1)
+
+	if ch := r.Header.Get("Content-Length"); ch != "" {
+		var err error
+		contentLength, err = strconv.ParseInt(ch, 10, 64)
+		if err != nil {
+			return err
+		}
+	}
+
 	for {
 		buf := make([]byte, chunksize)
 		n, err := io.ReadFull(r.Body, buf)
 		if n == 0 {
 			if err != nil && err != io.EOF {
-				fmt.Printf("err: %s", err)
+				f.deleteChunks(fi.Chunks)
+				return err
 			}
 			break
 		}
@@ -270,6 +311,7 @@ func (f *filerServer) handleUpload(w http.ResponseWriter, r *http.Request, path 
 
 		fid, err := f.uploadChunk(buf)
 		if err != nil {
+			f.deleteChunks(fi.Chunks)
 			return err
 		}
 		for _, v := range hashes {
@@ -281,17 +323,32 @@ func (f *filerServer) handleUpload(w http.ResponseWriter, r *http.Request, path 
 			Sha1sum: csum[:],
 		})
 	}
+	if contentLength >= 0 && fi.Size != contentLength {
+		f.deleteChunks(fi.Chunks)
+		return fmt.Errorf("size mismatch")
+	}
 	fi.Digests = make(map[string]string)
+	recvDigests := parseDigests(r.Header.Get("Digest"))
+	if ch := r.Header.Get("Content-MD5"); ch != "" {
+		recvDigests["MD5"] = ch
+	}
 	for k, v := range hashes {
 		fi.Digests[k] = base64.StdEncoding.EncodeToString(v.Sum(nil))
+		if prev, ok := recvDigests[k]; ok && fi.Digests[k] != prev {
+			f.deleteChunks(fi.Chunks)
+			return fmt.Errorf("%s checksum mismatch", k)
+		}
 	}
 	jb, err := json.Marshal(&fi)
 	if err != nil {
+		f.deleteChunks(fi.Chunks)
 		return err
 	}
 	if err = f.redisClient.Set(redisKey(path), jb, 0).Err(); err != nil {
+		f.deleteChunks(fi.Chunks)
 		return err
 	}
+	w.Write(jb)
 	return nil
 }
 
@@ -327,6 +384,7 @@ func main() {
 		}),
 		weedMaster: "http://localhost:9333",
 	}
+	fmt.Println(f.redisClient.Ping().Result())
 	http.Handle("/fs/", &f)
 	http.ListenAndServe(*listen, nil)
 }
