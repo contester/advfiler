9cca080e5c16c7e2693eccef62c7e60cfd5eef38
Paul Komkoff <i@stingr.net>
2018-09-21 17:23:53 +0400

    Update to envconfig

--- go.mod
+++ go.mod
@@ -1,10 +1,24 @@
 module git.stingr.net/stingray/advfiler
 
 require (
+	git.sgu.ru/sgu/systemdutil v0.0.0-20180813152046-df705fc6ca68
+	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
 	github.com/boltdb/bolt v1.3.1
+	github.com/coreos/go-systemd v0.0.0-20180828140353-eee3db372b31
 	github.com/gogo/protobuf v1.1.1
 	github.com/golang/protobuf v1.2.0
+	github.com/kelseyhightower/envconfig v1.3.0
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v0.8.0
+	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
+	github.com/prometheus/common v0.0.0-20180801064454-c7de2306084e // indirect
+	github.com/prometheus/procfs v0.0.0-20180920065004-418d78d0b9a7 // indirect
+	github.com/sirupsen/logrus v1.0.6
+	github.com/wercker/journalhook v0.0.0-20180428041537-5d0a5ae867b3
 	go.etcd.io/bbolt v1.3.0
+	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b // indirect
+	golang.org/x/net v0.0.0-20180921000356-2f5d2388922f
+	golang.org/x/sys v0.0.0-20180920110915-d641721ec2de // indirect
 	gopkg.in/bsm/ratelimit.v1 v1.0.0-20160220154919-db14e161995a // indirect
 	gopkg.in/redis.v4 v4.2.4
 )
--- main.go
+++ main.go
@@ -2,68 +2,70 @@ package main // import "git.stingr.net/stingray/advfiler"
 
 import (
 	"flag"
-	"log"
 	"net/http"
 	"time"
 
+	"git.sgu.ru/sgu/systemdutil"
+	"github.com/coreos/go-systemd/activation"
+	"github.com/coreos/go-systemd/daemon"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/wercker/journalhook"
+	"golang.org/x/net/trace"
+
+	log "github.com/sirupsen/logrus"
 	bolt "go.etcd.io/bbolt"
-	"gopkg.in/redis.v4"
+
+	_ "net/http/pprof"
 )
 
-var (
-	listen = flag.String("listen", ":9094", "Listen port")
-
-	weedBackend = flag.String("weed", "http://localhost:9333", "WeedFS backend")
-
-	redisBackend  = flag.String("redis", "localhost:6379", "Redis backend")
-	redisPassword = flag.String("redis_password", "", "Redis password")
-
-	redisDbFiles     = flag.Int("redis_db_files", 0, "Redis DB for files")
-	redisPrefixFiles = flag.String("redis_prefix_files", "fs/", "Prefix for redis keys for files")
-
-	redisDbProblems     = flag.Int("redis_db_problems", 0, "Redis DB for problems")
-	redisPrefixProblems = flag.String("redis_prefix_problems", "problem/", "Prefix for redis keys for problems")
-
-	boltDb = flag.String("bolt", "", "Bolt file name")
-)
+type conf3 struct {
+	BoltDB      string   `envconfig:"BOLT_DB"`
+	ListenHTTP  []string `envconfig:"LISTEN_HTTP"`
+	WeedBackend string   `envconfig:"WEED_BACKEND",default:"http://localhost:9333"`
+}
 
 func main() {
 	flag.Parse()
 
-	var fiKV, meKV filerKV
+	journalhook.Enable()
+	systemdutil.Logger = log.StandardLogger()
+	trace.AuthRequest = func(req *http.Request) (any, sensitive bool) { return true, true }
+	http.Handle("/metrics", prometheus.Handler())
 
-	if *boltDb != "" {
-		db, err := bolt.Open(*boltDb, 0600, &bolt.Options{Timeout: 1 * time.Second})
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
-		db.NoSync = true
-		fiKV = NewBoltKV(db, "fs")
-		meKV = NewBoltKV(db, "problems")
-	} else {
+	_, httpSockets, _ := systemdutil.ListenSystemd(activation.Files(true))
 
-		rc1 := redis.NewClient(&redis.Options{
-			Addr:     *redisBackend,
-			Password: *redisPassword,
-			DB:       *redisDbFiles,
-		})
-		defer rc1.Close()
+	var config conf3
 
-		rc2 := redis.NewClient(&redis.Options{
-			Addr:     *redisBackend,
-			Password: *redisPassword,
-			DB:       *redisDbProblems,
-		})
-		defer rc2.Close()
-		fiKV = NewRedisKV(rc1, *redisPrefixFiles)
-		meKV = NewRedisKV(rc2, *redisPrefixProblems)
+	if err := envconfig.Process("advfiler", &config); err != nil {
+		log.Fatal(err)
 	}
 
-	f := NewFiler(fiKV, &WeedClient{master: *weedBackend})
+	if config.BoltDB == "" {
+		log.Fatal("BOLT_DB needs to be specified")
+	}
+
+	httpSockets = append(httpSockets, systemdutil.MustListenTCPSlice(config.ListenHTTP)...)
+
+	var fiKV, meKV filerKV
+
+	db, err := bolt.Open(config.BoltDB, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	db.NoSync = true
+	fiKV = NewBoltKV(db, "fs")
+	meKV = NewBoltKV(db, "problems")
+
+	f := NewFiler(fiKV, &WeedClient{master: config.WeedBackend})
 	ms := NewMetadataServer(meKV)
 	http.Handle("/fs/", f)
 	http.HandleFunc("/problem/set/", ms.handleSetManifest)
 	http.HandleFunc("/problem/get/", ms.handleGetManifest)
-	http.ListenAndServe(*listen, nil)
+	for _, s := range httpSockets {
+		go http.Serve(s, nil)
+	}
+	daemon.SdNotify(false, "READY=1")
+	systemdutil.WaitSigint()
 }
