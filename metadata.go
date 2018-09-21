81acd7c364a2cef2702531083dc0b4c0e3c30204
Paul Komkoff <i@stingr.net>
2018-09-21 17:27:02 +0400

    Remove all traces of redis

--- metadata.go
+++ metadata.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"sort"
 	"strconv"
+
+	"github.com/golang/protobuf/proto"
 )
 
 type metadataServer struct {
@@ -157,4 +159,24 @@ func (f *metadataServer) handleGetManifest(w http.ResponseWriter, r *http.Reques
 		return
 	}
 	json.NewEncoder(w).Encode(revs)
+}
+
+type GetKV interface {
+	Get(ctx context.Context, key string) ([]byte, error)
+}
+
+func KVGetJson(ctx context.Context, kv GetKV, key string, value interface{}) error {
+	res, err := kv.Get(ctx, key)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(res, value)
+}
+
+func KVGetProto(ctx context.Context, kv GetKV, key string, value proto.Message) error {
+	res, err := kv.Get(ctx, key)
+	if err != nil {
+		return err
+	}
+	return proto.Unmarshal(res, value)
 }
